9e7c32036c4c8f5041c8ac3cf649bbfb334afae5
joannaatento <06802182@dwc-legazpi.edu>
2023-02-21 20:04:07 +0800

    homepagelangview

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,18 +1,67 @@
-import { Component} from '@angular/core';
-import { Auth } from '@angular/fire/auth';
-import { Database} from '@angular/fire/database';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/compat/database';
+import { Database, ref, set, update, remove,query,orderByChild,equalTo} from '@angular/fire/database';
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/database'
+import { getDatabase, onValue} from "firebase/database";
+import { Observable } from 'rxjs';
+
+
+
+interface Item {
+  password: string;
+}
+
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
- 
-    constructor(public auth: Auth,public database:Database,private router:Router) { }
-  
-    ngOnInit(): void {
-    }
+export class HomepageComponent implements OnInit {
 
-}
+  users!: Observable<any[]>;
+  constructor(public database: Database, private db: AngularFireDatabase) {
+  this.users = db.list('/users').valueChanges();
+   }
+   
+  ngOnInit(): void {
+
+
+  }
+
+
+  del(value: any){
+    remove(ref(this.database, 'users/' + value));
+    alert('Deleted Successfully')
+  }
+
+   update(value:any){
+ 
+    update(ref(this.database, 'users/' + value.email), {
+       password: value.password
+     }); 
+    alert('User updated!');
+      
+  }
+
+
+  email = '';
+  fillForm(email: any) {
+    this.email = email;
+  }
+
+
+
+  password: any;
+  itemId: any;
+
+ 
+  }
+
+ 
+
+
+
+
+  
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit} from '@angular/core';
 import { Auth, signInWithEmailAndPassword} from '@angular/fire/auth';
-import { Database, ref, update } from '@angular/fire/database';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Database, ref, update,onValue, remove} from '@angular/fire/database';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,36 +12,65 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit{
 
-  constructor(public auth: Auth,public database:Database,private router:Router) { }
-
+  users!: Observable<any[]>;
+  constructor(public auth: Auth,public database: Database, private db: AngularFireDatabase,private router:Router) {
+    this.users = db.list('/users').valueChanges();
+   }
+   
   ngOnInit(): void {
+
+
   }
 
   loginUser(value: any) {
 
-    signInWithEmailAndPassword(this.auth, value.email, value.password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-
-        alert('user login');
-        const date = new Date();
-        update(ref(this.database, 'users/' + user.uid), {
-
-          last_login: date
-        });
-
-        this.router.navigate(['/signup'])
+    //login 
+       signInWithEmailAndPassword(this.auth, value.email, value.password)
+        .then((userCredential) => {
       
-    },err=>{
-      alert(err.message)
+         const user = userCredential.user;
+
+         alert('user login');
+          const date = new Date();
+          update(ref(this.database, 'users/' + user.uid), {
+
+         last_login: date
+       });
+
+       this.router.navigate(['/signup'])
+      
+  },err=>{
+     alert(err.message)
   
-    })
+   })
       
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+    .catch((error) => {
+     const errorCode = error.code;
+     const errorMessage = error.message;
+    });
 
-      });
+    //read user
+
+    //  const starCountRef = ref(this.database, 'users/' + value.email);
+     // onValue(starCountRef, (snapshot) => {
+      //const data = snapshot.val();
+   
+     // alert(data.email);
+   
+      //update user
+
+//update(ref(this.database, 'users/' + value.email), {
+//password: value.password
+//});
+//alert('password updated!');
+
+//remove user
+
+//remove(ref(this.database, 'users/' + value.email));
+//alert('Successfully Removed');
+
+
   }
+
+  
 }
